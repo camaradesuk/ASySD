cf96705d261667678ca4518dc0af204fb04ae8e0
kaitlynhair <kaitlyn.hair@ed.ac.uk>
2022-12-07 16:32:40 +0000

    add validation

---
+++ validation/Check_performance.R
@@ -0,0 +1,24 @@
+library(osfr)
+source("~/Projects/ASySD/validation/calculate_performance_function.R")
+
+# get labelled duplicate sets
+asysd_validation_project <- osf_retrieve_node("https://osf.io/2b8uq/")
+files <- osf_ls_files(asysd_validation_project)
+osf_download(files[7,], path="validation")
+
+# used to validate and check false positives/negatives for ASySD v2
+SRSR_duplicates_labelled <- read_csv("validation/duplicates_labelled/SRSR_duplicates_labelled.csv", col_types = cols(record_id = col_character()))
+Diabetes_duplicates_labelled <- read_csv("validation/duplicates_labelled/Diabetes_duplicates_labelled.csv", col_types = cols(record_id = col_character()))
+
+# test sets for ASySD v2 (not used for further developments)
+NeuroImaging_duplicates_labelled <- read_csv("validation/duplicates_labelled/NeuroImaging_duplicates_labelled.csv", col_types = cols(record_id = col_character()))
+Cardiac_duplicates_labelled <- read_csv("validation/duplicates_labelled/Cardiac_duplicates_labelled.csv", col_types = cols(record_id = col_character()))
+Depression_duplicates_labelled <- read_csv("validation/duplicates_labelled/Depression_duplicates_labelled.csv", col_types = cols(record_id = col_character()))
+
+calculate_performance(SRSR_duplicates_labelled)
+calculate_performance(NeuroImaging_duplicates_labelled)
+calculate_performance(Cardiac_duplicates_labelled)
+calculate_performance(Depression_duplicates_labelled)
+calculate_performance(Diabetes_duplicates_labelled)
+
+
---
+++ validation/calculate_performance_function.R
@@ -0,0 +1,41 @@
+calculate_performance <- function(data){
+
+  asysd_result <- dedup_citations(data, keep_label = "Unique", merge_citations = FALSE)
+  unique <- asysd_result$unique
+
+  unique_to_match <- unique %>%
+    rename(asysd_label = label) %>%
+    select(asysd_label, record_id)
+
+  check <- left_join(data, unique_to_match, by="record_id")
+  check <- check %>%
+    mutate(asysd_label = ifelse(is.na(asysd_label), "Duplicate", paste(asysd_label)))
+
+  summary <- check %>%
+    ungroup() %>%
+    count(label, asysd_label)
+
+  true_neg <- as.numeric(summary$n[which(summary$label == "Unique" & summary$asysd_label == "Unique")])
+  false_neg <- as.numeric(summary$n[which(summary$label == "Duplicate" & summary$asysd_label == "Unique")])
+  true_pos <- as.numeric(summary$n[which(summary$label == "Duplicate" & summary$asysd_label == "Duplicate")])
+  false_pos <- as.numeric(summary$n[which(summary$label == "Unique" & summary$asysd_label == "Duplicate")])
+
+  if(length(false_neg) == 0){
+    false_neg <- 0
+  }else{
+    false_neg <- false_neg
+  }
+
+  if(length(false_pos) == 0){
+    false_pos <- 0
+    }else{
+      false_pos <- false_pos
+  }
+
+  sensitivity <- true_pos / (true_pos + false_neg) * 100
+  specificity <- true_neg / (true_neg + false_pos) * 100
+
+  data.frame(Measure= c("True_neg", "True_pos", "False_neg", "False_pos", "Sensitivity", "Specificity"),
+   Result = c(true_neg, true_pos, false_neg, false_pos, sensitivity, specificity))
+
+  }
