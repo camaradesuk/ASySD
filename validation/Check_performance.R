9a787aab28b47a8cffc78d141b2b074a830b9fdc
kaitlynhair <kaitlyn.hair@ed.ac.uk>
2023-02-24 17:34:53 +0000

    validation checks

--- validation/Check_performance.R
+++ validation/Check_performance.R
@@ -1,26 +1,36 @@
 library(osfr)
-source("~/Projects/ASySD/validation/calculate_performance_function.R")
+source("validation/calculate_performance_function.R")
 library(readr)
 library(dplyr)
 library(ASySD)
 
-# get labelled duplicate sets
+# get labelled duplicate sets from OSF project
 asysd_validation_project <- osf_retrieve_node("https://osf.io/2b8uq/")
 files <- osf_ls_files(asysd_validation_project)
 osf_download(files[7,], path="validation")
 
-# used to validate and check false positives/negatives for ASySD v2
+# used to validate and check initial ASySD version #1
 SRSR_duplicates_labelled <- read_csv("validation/duplicates_labelled/SRSR_duplicates_labelled.csv", col_types = cols(record_id = col_character()))
 Diabetes_duplicates_labelled <- read_csv("validation/duplicates_labelled/Diabetes_duplicates_labelled.csv", col_types = cols(record_id = col_character()))
-
-# test sets for ASySD v2 (not used for further developments)
 NeuroImaging_duplicates_labelled <- read_csv("validation/duplicates_labelled/NeuroImaging_duplicates_labelled.csv", col_types = cols(record_id = col_character()))
 Cardiac_duplicates_labelled <- read_csv("validation/duplicates_labelled/Cardiac_duplicates_labelled.csv", col_types = cols(record_id = col_character()))
 Depression_duplicates_labelled <- read_csv("validation/duplicates_labelled/Depression_duplicates_labelled.csv", col_types = cols(record_id = col_character()))
 
-calculate_performance(SRSR_duplicates_labelled)
-calculate_performance(NeuroImaging_duplicates_labelled)
-calculate_performance(Cardiac_duplicates_labelled)
-calculate_performance(Depression_duplicates_labelled)
-calculate_performance(Diabetes_duplicates_labelled)
+
+# check performance hasn't changed substantially or has only improved
+# check both types of dedup - merge and keep one
+calculate_performance_1(SRSR_duplicates_labelled)
+calculate_performance_2(SRSR_duplicates_labelled)
+
+calculate_performance_1(NeuroImaging_duplicates_labelled)
+calculate_performance_2(NeuroImaging_duplicates_labelled)
+
+calculate_performance_1(Cardiac_duplicates_labelled)
+calculate_performance_2(Cardiac_duplicates_labelled)
+
+calculate_performance_1(Diabetes_duplicates_labelled)
+calculate_performance_2(Diabetes_duplicates_labelled)
+
+calculate_performance_1(Depression_duplicates_labelled)
+calculate_performance_2(Depression_duplicates_labelled)
 
--- validation/calculate_performance_function.R
+++ validation/calculate_performance_function.R
@@ -1,4 +1,4 @@
-calculate_performance <- function(data){
+calculate_performance_1 <- function(data){
 
   asysd_result <- dedup_citations(data, keep_label = "Unique", merge_citations = FALSE)
   unique <- asysd_result$unique
@@ -40,3 +40,48 @@ calculate_performance <- function(data){
    Result = c(true_neg, true_pos, false_neg, false_pos, sensitivity, specificity))
 
   }
+
+calculate_performance_2 <- function(data){
+
+  asysd_result <- dedup_citations(data, keep_label = "Unique", merge_citations = TRUE)
+  unique <- asysd_result$unique
+
+  unique_to_match <- unique %>%
+    rename(asysd_label = label) %>%
+    mutate(asysd_label = "Unique") %>%
+    select(asysd_label, duplicate_id)
+
+  check <- left_join(data, unique_to_match, by=c("record_id"="duplicate_id"))
+
+  check <- check %>%
+    mutate(asysd_label = ifelse(is.na(asysd_label), "Duplicate", paste(asysd_label)))
+
+  summary <- check %>%
+    ungroup() %>%
+    count(label, asysd_label)
+
+  true_neg <- as.numeric(summary$n[which(summary$label == "Unique" & summary$asysd_label == "Unique")])
+  false_neg <- as.numeric(summary$n[which(summary$label == "Duplicate" & summary$asysd_label == "Unique")])
+  true_pos <- as.numeric(summary$n[which(summary$label == "Duplicate" & summary$asysd_label == "Duplicate")])
+  false_pos <- as.numeric(summary$n[which(summary$label == "Unique" & summary$asysd_label == "Duplicate")])
+
+  if(length(false_neg) == 0){
+    false_neg <- 0
+
+  } else{
+    false_neg <- false_neg
+  }
+
+  if(length(false_pos) == 0){
+    false_pos <- 0
+  }else{
+    false_pos <- false_pos
+  }
+
+  sensitivity <- true_pos / (true_pos + false_neg) * 100
+  specificity <- true_neg / (true_neg + false_pos) * 100
+
+  data.frame(Measure= c("True_neg", "True_pos", "False_neg", "False_pos", "Sensitivity", "Specificity"),
+             Result = c(true_neg, true_pos, false_neg, false_pos, sensitivity, specificity))
+
+}
