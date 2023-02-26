4a64d8ae4b31bce1f5a0558850b8cce24d4e5f17
kaitlynhair <kaitlyn.hair@ed.ac.uk>
2023-02-26 13:29:50 +0000

    add export function for flagged dups in app

--- R/export_refs.R
+++ R/export_refs.R
@@ -79,3 +79,114 @@ write_citations <- function(citations, type=c("ris", "txt", "csv", "bib"), filen
                                    file = filename)
   }
 }
+
+#' This function writes citation data to disk in different formats
+#' @export
+#' @import dplyr
+#' @import synthesisr
+#' @param citations A dataframe containing citations - usually post-deduplication
+#' @param type export type
+#' @return file export
+write_citations_app <- function(citations, type=c("ris", "txt", "csv", "bib"), filename){
+
+  if(type == "txt"){
+
+
+    citations$`Custom 1` <- citations$duplicate_id
+    citations$`Custom 2` <- ""
+
+    if("flag" %in% colnames(citations))
+    {
+      citations$`Custom 2` <- citations$flag
+    }
+
+
+    refs <- citations %>%
+      mutate("Reference Type" = "Journal Article") %>%
+      rename("ISBN/ISSN" = isbn,
+             "Author" = author,
+             "Title" = title,
+             "Volume" = volume,
+             "Number" = number,
+             "Label" = label,
+             "Year" = year,
+             "Abstract" = abstract,
+             "Pages" = pages,
+             "DOI" = doi,
+             "Secondary Title" = journal) %>%
+      ungroup() %>%
+      select("Reference Type", "Author", "Year",
+             "Secondary Title", "DOI", "Title",
+             "Pages", "Volume", "Number", "Abstract",
+             "Custom 1", "Custom 2","ISBN/ISSN", "Label")
+
+    refs[] <- lapply(refs, function(x) gsub("\\r\\n|\\r|\\n", "", x))
+
+    write.table(refs, filename, sep="\t",
+                col.names=TRUE, row.names = F, quote=FALSE, na="")
+
+  } else if(type == "csv"){
+
+    refs <- citations %>%
+      rename(Authors = author,
+             Title = title,
+             Abstract = abstract,
+             Year = year,
+             DOI= doi,
+             PublicationName = journal)  %>%
+      mutate(Url = "",
+             AuthorAddress = "",
+             AlternateName = "",
+             ReferenceType = "",
+             CustomId = duplicate_id,
+             Keywords = "",
+             PdfRelativePath = paste0(duplicate_id, ".pdf")) %>%
+      select(Title,
+             Authors,
+             PublicationName,
+             AlternateName,
+             Abstract,
+             Url,
+             AuthorAddress,
+             Year,
+             DOI,
+             ReferenceType,
+             Keywords,
+             PdfRelativePath,
+             CustomId)
+
+    write.csv(refs, filename,
+              col.names=TRUE, row.names = F, quote=FALSE, na="")
+
+  } else if(type == "ris"){
+
+    citations <- as.data.frame(citations)
+    citations$database <- citations$duplicate_id
+    citations$notes <- ""
+
+    if("flag" %in% colnames(citations))
+    {
+      citations$notes <- citations$flag
+    }
+
+    synthesisr::write_refs(citations,
+                           format = "ris",
+                           file = filename
+    )
+  } else if(type == "bib"){
+
+    citations <- as.data.frame(citations)
+    citations$database <- citations$duplicate_id
+    citations$notes <- ""
+
+    if("flag" %in% colnames(citations))
+    {
+      citations$notes <- citations$flag
+    }
+
+    refs <- synthesisr::write_refs(citations, format = "bib",
+                                   file = filename)
+  }
+}
+
+
