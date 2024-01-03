fde700f180e93edaa20f0c556163f36e56e7ee92
kaitlynhair <kaitlyn.hair@ed.ac.uk>
2024-01-03 16:06:00 +0000

    adding additional tests for when cols missing

--- tests/testthat/test-dedup.R
+++ tests/testthat/test-dedup.R
@@ -7,15 +7,16 @@ test_that("dedup_check", {
   # when no dups, check warning works
   test_citations_nodups <- test_citations[c(6,21,65),]
 
-  # duplicate pairs detected but no TRUE pairs
-  test_citations_notruedups <- test_citations[c(5,11,13),]
-  test_citations_notruedups$doi <- test_citations_notruedups$doi[2]
-
   expect_warning(
     a <- dedup_citations(test_citations_nodups, merge_citations = TRUE), "No duplicates detected!")
 
   expect_warning(
     a <- dedup_citations(test_citations_nodups, merge_citations = FALSE), "No duplicates detected!")
+
+
+  # duplicate pairs detected but no TRUE pairs
+  test_citations_notruedups <- test_citations[c(5,11,13),]
+  test_citations_notruedups$doi <- test_citations_notruedups$doi[2]
 
   expect_warning(
     a <- dedup_citations(test_citations_notruedups, merge_citations = TRUE), "No duplicates detected!")
@@ -23,6 +24,26 @@ test_that("dedup_check", {
   expect_warning(
     a <- dedup_citations(test_citations_notruedups, merge_citations = FALSE), "No duplicates detected!")
 
+  # when non-essential column missing
+  test_citations_nolabel <- test_citations %>% select(-label)
+
+  expect_no_warning(
+    a <- dedup_citations(test_citations_nolabel, merge_citations = FALSE))
+
+  # when essential column missing
+  test_citations_nopages <- test_citations %>% select(-pages)
+
+  expect_warning(regexp = "The following columns are missing: pages",
+                 dedup_citations(test_citations_nopages, merge_citations = FALSE, user_input = 1)
+  )
+
+  res <- dedup_citations(test_citations_nopages, merge_citations = FALSE, user_input = 1)
+  expect_equal(
+    length(res$unique$duplicate_id), 584)
+
+  expect_match(regexp = "Halting dedup",
+    dedup_citations(test_citations_nopages, merge_citations = FALSE, user_input = 2)
+  )
 
   # check deduplication performing as normal
   res <- dedup_citations(test_citations, merge_citations = FALSE, manual_dedup = FALSE)
