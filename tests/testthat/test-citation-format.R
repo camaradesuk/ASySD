19740c33fa13ac58c4f70f85fd93b70c69f6af19
kaitlynhair <kaitlyn.hair@ed.ac.uk>
2024-01-03 16:30:20 +0000

    splitting up test functions to run independently

--- tests/testthat/test-citation-format.R
+++ tests/testthat/test-citation-format.R
@@ -1,46 +1,65 @@
-test_that("citations_check", {
+library(testthat)
+library(dplyr)
 
-  # get testing set
-  test_citations <- read.csv("labelled_test_set.csv")
-  test_citations <- as.data.frame(test_citations)
+# Load the testing set
+test_citations <- read.csv("labelled_test_set.csv")
+test_citations <- as.data.frame(test_citations)
 
-  # create set with no record_id col
+# Test case: No record_id column
+test_that("No record_id column", {
   test_citations_no_col_for_id <- test_citations %>%
     select(-record_id)
 
-  # create set with all blank record_id col
+  w <- capture_warnings(dedup_citations(test_citations_no_col_for_id, merge_citations = TRUE, user_input = 1))
+  expect_match(w, "Search contains missing values for the record_id column. A record_id will be created using row numbers", all = FALSE)
+})
+
+# Test case: Blank record_id column
+test_that("Blank record_id column", {
   test_citations_no_record_id <- test_citations
   test_citations_no_record_id$record_id <- ""
 
-  # create set with all NA record_id col
+  expect_warning(
+    dedup_citations(test_citations_no_record_id, merge_citations = TRUE),
+    "Search contains missing values for the record_id column. A record_id will be created using row numbers"
+  )
+})
+
+# Test case: NA record_id column
+test_that("NA record_id column", {
   test_citations_na_record_id <- test_citations
   test_citations_na_record_id$record_id <- NA
 
-  # create set with non unique record_id col
-  test_citations_non_unique <- test_citations
-  test_citations_non_unique$record_id <- rep(c(1:10), times=round(nrow(test_citations)), length.out = nrow(test_citations))
-
-  # create set with some missing record_id
-  test_citations_missing <- test_citations
-  test_citations_missing$record_id <- rep(c(1, 2, 3, 4, NA), times=round(nrow(test_citations)), length.out = nrow(test_citations))
-
   expect_warning(
-     dedup_citations(test_citations_no_col_for_id, merge_citations = TRUE), "Search does not contain a record_id column. A record_id will be created using row numbers")
-
-  expect_warning(
-      dedup_citations(test_citations_no_record_id, merge_citations = TRUE),"Search contains missing values for the record_id column. A record_id will be created using row numbers")
-
-  expect_warning(
-      dedup_citations(test_citations_na_record_id, merge_citations = TRUE), "Search contains missing values for the record_id column. A record_id will be created using row numbers")
-
-  expect_warning(
-      dedup_citations(test_citations_non_unique, merge_citations = TRUE), "The record_id column is not unique. A record_id will be created using row numbers")
-
-  expect_warning(
-      dedup_citations(test_citations_missing, merge_citations = TRUE), "Search contains missing values for the record_id column. A record_id will be created using row numbers")
-
-  # check formatting citations doesn't drop rows
-  expect_equal(nrow(ASySD:::format_citations(test_citations)), nrow(test_citations))
+    dedup_citations(test_citations_na_record_id, merge_citations = TRUE, user_input = 1),
+    "Search contains missing values for the record_id column. A record_id will be created using row numbers"
+  )
 })
 
+# Test case: Non-unique record_id column
+test_that("Non-unique record_id column", {
+  test_citations_non_unique <- test_citations
+  test_citations_non_unique$record_id <- rep(c(1:10), times = round(nrow(test_citations)), length.out = nrow(test_citations))
+
+  expect_warning(
+    dedup_citations(test_citations_non_unique, merge_citations = TRUE),
+    "The record_id column is not unique. A record_id will be created using row numbers"
+  )
+})
+
+# Test case: Some missing record_id values
+test_that("Some missing record_id values", {
+  test_citations_missing <- test_citations
+  test_citations_missing$record_id <- rep(c(1, 2, 3, 4, NA), times = round(nrow(test_citations)), length.out = nrow(test_citations))
+
+  expect_warning(
+    dedup_citations(test_citations_missing, merge_citations = TRUE),
+    "Search contains missing values for the record_id column. A record_id will be created using row numbers"
+  )
+})
+
+# Test case: Formatting citations doesn't drop rows
+test_that("Formatting citations doesn't drop rows", {
+  expect_equal(nrow(ASySD:::format_citations(test_citations)), nrow(test_citations))
+})
 
--- tests/testthat/test-dedup.R
+++ tests/testthat/test-dedup.R
@@ -1,59 +1,66 @@
-test_that("dedup_check", {
+library(testthat)
+library(dplyr)
 
-  # get testing set
-  test_citations <- read.csv("labelled_test_set.csv")
-  test_citations <- as.data.frame(test_citations)
+# Load the testing set
+test_citations <- read.csv("labelled_test_set.csv")
+test_citations <- as.data.frame(test_citations)
 
-  # when no dups, check warning works
-  test_citations_nodups <- test_citations[c(6,21,65),]
+# Test case: No duplicates detected
+test_that("No duplicates detected", {
+  test_citations_nodups <- test_citations[c(6, 21, 65),]
 
-  expect_warning(
-    a <- dedup_citations(test_citations_nodups, merge_citations = TRUE), "No duplicates detected!")
+  expect_warning({
+    a <- dedup_citations(test_citations_nodups, merge_citations = TRUE)
+  }, "No duplicates detected!")
 
-  expect_warning(
-    a <- dedup_citations(test_citations_nodups, merge_citations = FALSE), "No duplicates detected!")
+  expect_warning({
+    a <- dedup_citations(test_citations_nodups, merge_citations = FALSE)
+  }, "No duplicates detected!")
+})
 
-
-  # duplicate pairs detected but no TRUE pairs
-  test_citations_notruedups <- test_citations[c(5,11,13),]
+# Test case: Duplicate pairs detected but no TRUE pairs
+test_that("Duplicate pairs detected but no TRUE pairs", {
+  test_citations_notruedups <- test_citations[c(5, 11, 13),]
   test_citations_notruedups$doi <- test_citations_notruedups$doi[2]
 
-  expect_warning(
-    a <- dedup_citations(test_citations_notruedups, merge_citations = TRUE), "No duplicates detected!")
+  expect_warning({
+    a <- dedup_citations(test_citations_notruedups, merge_citations = TRUE)
+  }, "No duplicates detected!")
 
-  expect_warning(
-    a <- dedup_citations(test_citations_notruedups, merge_citations = FALSE), "No duplicates detected!")
+  expect_warning({
+    a <- dedup_citations(test_citations_notruedups, merge_citations = FALSE)
+  }, "No duplicates detected!")
+})
 
-  # when non-essential column missing
+# Test case: Non-essential column missing
+test_that("Non-essential column missing", {
   test_citations_nolabel <- test_citations %>% select(-label)
 
-  expect_no_warning(
-    a <- dedup_citations(test_citations_nolabel, merge_citations = FALSE))
+  expect_no_warning({
+    a <- dedup_citations(test_citations_nolabel, merge_citations = FALSE)
+  })
+})
 
-  # when essential column missing
+# Test case: Essential column missing
+test_that("Essential column missing", {
   test_citations_nopages <- test_citations %>% select(-pages)
 
-  expect_warning(regexp = "The following columns are missing: pages",
-                 dedup_citations(test_citations_nopages, merge_citations = FALSE, user_input = 1)
-  )
-
   res <- dedup_citations(test_citations_nopages, merge_citations = FALSE, user_input = 1)
-  expect_equal(
-    length(res$unique$duplicate_id), 584)
+  expect_equal(length(res$unique$duplicate_id), 584)
 
-  expect_match(regexp = "Halting dedup",
+  expect_match({
     dedup_citations(test_citations_nopages, merge_citations = FALSE, user_input = 2)
-  )
+  }, regexp = "Halting dedup")
+})
 
-  # check deduplication performing as normal
+# Test case: Deduplication performing as normal
+test_that("Deduplication performing as normal", {
   res <- dedup_citations(test_citations, merge_citations = FALSE, manual_dedup = FALSE)
+  expect_equal(length(res$duplicate_id), 586)
+})
 
-  expect_equal(
-    length(res$duplicate_id), 586)
-
-  # check deduplication with merge performing as normal
-  res <- dedup_citations(test_citations, merge_citations=TRUE)
-
-  expect_equal(
-  length(res$unique$duplicate_id), 586)
+# Test case: Deduplication with merge performing as normal
+test_that("Deduplication with merge performing as normal", {
+  res <- dedup_citations(test_citations, merge_citations = TRUE)
+  expect_equal(length(res$unique$duplicate_id), 586)
 })
