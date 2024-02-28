bc896600cc0b1d1103f69d156db8af9071165423
kaitlynhair <kaitlyn.hair@ed.ac.uk>
2024-02-28 18:19:28 +0000

    Improve documentation and simplify

--- R/batch_dedup.R
+++ R/batch_dedup.R
@@ -1,53 +1,68 @@
-####------ Deduplicate large searches ------ ####
-#' Get new unique citations and perform deduplication
-#'
-#' This function processes a dataframe of citations, deduplicates them, and provides unique citation results.
+####------ Deduplicate searches in batches ------ ####
+
+#' This function processes a dataframe of citations, deduplicates them, and provides unique citation results. If the number of citations exceeds a specified threshold, it splits the dataframe and performs deduplication on each batch.
 #'
 #' @param citations A dataframe containing citation information.
-#' @param keep_source Character vector. Selected citation source to preferentially retain in the dataset as the unique record
-#' @param keep_label Selected citation label to preferentially retain in the dataset as the unique record
-#'  @import dplyr
+#' @param batch_n Numeric value specifying the maximum number of citations per batch. Default is 50000.
+#' @param keep_source Character vector specifying the citation source(s) to preferentially retain in the dataset as the unique record.
+#' @param keep_label Character vector specifying the citation label(s) to preferentially retain in the dataset as the unique record.
+#' @param sort_by Character vector specifying the sorting criteria. Default is c("year", "title","author"). Valid options are column names of the citations dataframe.
 #' @return A list with components:
 #'   \describe{
 #'     \item{unique}{Dataframe containing unique citations.}
 #'     \item{manual_dedup}{Dataframe containing citations to be manually checked for duplicates.}
 #'   }
+#' @import dplyr
 #' @export
 
-dedup_large_search <- function(citations, keep_source=NULL, keep_label=NULL){
+batch_dedup <- function(citations, batch_n = 50000, keep_source=NULL, keep_label=NULL, sort_by = c("year", "title","author")){
 
-  if(length(citations$record_id) > 50000){
+  message("Splitting up dataframe")
 
-    message("Splitting up dataframe and running multiple deduplications due to size...")
+  unique <- list()
+  manual <- list()
 
-    unique <- list()
-    manual <- list()
+  # Convert sortby to lowercase to match column names
+  sort_by <- tolower(sort_by)
 
-    citations <- citations %>%
-      arrange(year, title, author)
+  # Check if sortby values are valid column names
+  valid_sortby <- sort_by[sort_by %in% names(citations)]
 
-    split_citations <- split(citations, ceiling(seq(nrow(citations))/50000))
+  if (length(valid_sortby) == 0) {
+    stop("Invalid sort_by criteria. Please provide valid column names.")
+  }
 
-    for(i in 1:length(split_citations)){
+  # Arrange dataframe based on sortby criteria
+  citations <- citations %>%
+    arrange_at(valid_sortby)
 
-      # Perform deduplication for the current year's citations
-      dedup_results <- ASySD::dedup_citations(split_citations[[i]], merge_citations = TRUE,
-                                              keep_source = keep_source, keep_label = keep_label)
 
-      # Append deduplicated results to the list
-      unique[[i]] <- dedup_results$unique
-      manual[[i]] <- dedup_results$manual
-    }
+  split_citations <- split(citations, ceiling(seq(nrow(citations))/batch_n))
 
-    res_unique <- bind_rows(unique)
-    res_manual <- bind_rows(manual)
+  for(i in 1:length(split_citations)){
 
-  } else {
+    suppressWarnings({
+      suppressMessages({
+        # Perform deduplication for the current year's citations
+        dedup_results <- dedup_citations(split_citations[[i]], merge_citations = TRUE,
+                                         keep_source = keep_source, keep_label = keep_label)
+      })
+    })
 
-    dedup_results <- ASySD::dedup_citations(citations, merge_citations = TRUE, keep_source = keep_source)
+    # Append deduplicated results to the list
+    unique[[i]] <- dedup_results$unique
+    manual[[i]] <- dedup_results$manual
+    message(paste0("batch ", i, " complete \U2714"))
+  }
 
-    res_unique <- dedup_results$unique
-    res_manual <- dedup_results$manual
+  res_unique <- bind_rows(unique)
+  res_manual <- bind_rows(manual)
+
+
+  if ("record_id" %in% colnames(res_unique)){
+
+    res_unique <- res_unique %>%
+      select(-record_id)
 
   }
 
@@ -65,4 +80,3 @@ dedup_large_search <- function(citations, keep_source=NULL, keep_label=NULL){
               "manual_dedup" = res_manual))
 
 }
-
