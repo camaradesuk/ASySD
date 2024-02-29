76e220eae99bab5b501d6584b5ce54aeff9ae599
kaitlynhair <kaitlyn.hair@ed.ac.uk>
2024-02-29 09:55:43 +0000

    add data documentation

--- R/data.R
+++ R/data.R
@@ -1,18 +1,17 @@
 #' Field conversion codes for pubmed imports
 #'
-#' @name field_codes_pubmed
 #' @docType data
 #' @author Kaitlyn Hair \email{kaitlyn.hair@ed.ac.uk}
+"field_codes_pubmed"
 
 #' Field conversion codes for Web of Science imports
 #'
-#' @name field_codes_wos
 #' @docType data
 #' @author Kaitlyn Hair \email{kaitlyn.hair@ed.ac.uk}
-
+"field_codes_wos"
 
 #' Exmaple citation data
 #'
-#' @name citations
 #' @docType data
+"citations_df"
 
